19f96f34c5c38ab8d819e80a1ffa81342d9caba6
一只小凌云 <1913532130@qq.com>
2023-09-17 05:46:07 +0000

    update EditGuide/editguide.js.
    Signed-off-by: 一只小凌云 <1913532130@qq.com>

--- EditGuide/editguide.js
+++ EditGuide/editguide.js
@@ -1,8 +1,8 @@
 /*   #1   */
 
 function setuparg(){
-	var ip   = ''  //此处填写你指定的服务器 IP | Write your Server IP Here
-	var port = ''  //此处填写你指定的服务器端口，若无则填写 25565 | Write your Server Port Here , if you don't know use 25565
+	var ip   = ''  // 此处填写你指定的服务器 IP | Write your Server IP Here
+	var port = ''  // 此处填写你指定的服务器端口，若无则填写 25565 | Write your Server Port Here , if you don't know use 25565
 	reloadall(ip,port)
 }
 
